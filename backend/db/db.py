6cb5a92c85843917e3dbe23af5565002047b614a
frenchytheasian <michael.x.french@gmail.com>
2021-10-16 10:48:22 -0500

    backend updates

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from pprint import pprint
+from settings import mongo_uri
 
-
-client = pymongo.MongoClient("mongodb+srv://dbUser:<password>@cluster0.n2dm7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.test
+uri = mongo_uri()
+print(uri)
--- server/db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-from pprint import pprint
-
-
-client = pymongo.MongoClient("mongodb+srv://dbUser:<password>@cluster0.n2dm7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.test
