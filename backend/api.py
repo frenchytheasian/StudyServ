b0bdf1291c15ff8fc97bd3dce8f5dd67f2fb0b3d
SamMsen03 <mrwaddlesam@gmail.com>
2021-10-16 21:02:22 -0500

    backend updates

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,22 @@
 import db
 import flask
+from flask import Flask, jsonify, request
 
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+
+api = Api(app)
+
+
+
+def get(self):
+    return jsonify({'message': 'hello world'})
+def post(self):
+    data = request.get_json()
+    return jsonify({'data': data}), 201
+
+
+
+if _name_ == '__main__':
+    app.run(debug = True)
